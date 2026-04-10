c2a558116594eb10048302232e45ec690a57bef2
evierhys13-web <evierhys13@gmail.com>
2026-04-10 07:48:11 +0100

    Fix Dockerfile with --omit=dev

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
-  },
-  preview: {
-    port: 3000,
-    host: true
-  },
+  base: './',
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    minify: 'terser'
   }
 })
