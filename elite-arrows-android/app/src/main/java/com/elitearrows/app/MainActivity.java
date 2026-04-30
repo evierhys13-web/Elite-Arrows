d5d25fc8bdeb0e23411fafdeeeba9ebdc3ee5baa
evierhys13-web <evierhys13@gmail.com>
2026-04-30 15:21:43 +0100

    Enable Android app file uploads

--- elite-arrows-android/app/src/main/java/com/elitearrows/app/MainActivity.java
+++ elite-arrows-android/app/src/main/java/com/elitearrows/app/MainActivity.java
@@ -1,9 +1,17 @@
 package com.elitearrows.app;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -11,14 +19,24 @@ import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.google.firebase.FirebaseApp;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView;
     private ProgressBar mProgressBar;
+    private ValueCallback<Uri[]> filePathCallback;
+    private Uri cameraPhotoUri;
     private static final String BASE_URL = "https://elite-arrows.co.uk";
+    private static final int FILE_CHOOSER_REQUEST_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,9 +98,97 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(
+                    WebView webView,
+                    ValueCallback<Uri[]> filePathCallback,
+                    WebChromeClient.FileChooserParams fileChooserParams
+            ) {
+                if (MainActivity.this.filePathCallback != null) {
+                    MainActivity.this.filePathCallback.onReceiveValue(null);
+                }
+                MainActivity.this.filePathCallback = filePathCallback;
+
+                Intent fileIntent;
+                try {
+                    fileIntent = fileChooserParams.createIntent();
+                } catch (Exception e) {
+                    fileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                    fileIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                    fileIntent.setType("image/*");
+                }
+
+                try {
+                    Intent cameraIntent = createCameraIntent();
+                    if (fileChooserParams.isCaptureEnabled() && cameraIntent != null) {
+                        startActivityForResult(cameraIntent, FILE_CHOOSER_REQUEST_CODE);
+                    } else {
+                        startActivityForResult(fileIntent, FILE_CHOOSER_REQUEST_CODE);
+                    }
+                } catch (ActivityNotFoundException e) {
+                    MainActivity.this.filePathCallback = null;
+                    return false;
+                }
+                return true;
+            }
+        });
+
         // Load URL with cache-busting query param
         long timestamp = System.currentTimeMillis();
         mWebView.loadUrl(BASE_URL + "?v=" + timestamp);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode != FILE_CHOOSER_REQUEST_CODE || filePathCallback == null) {
+            return;
+        }
+
+        Uri[] results = null;
+        if (resultCode == Activity.RESULT_OK) {
+            if (data == null || data.getData() == null) {
+                if (cameraPhotoUri != null) {
+                    results = new Uri[] { cameraPhotoUri };
+                }
+            } else {
+                results = WebChromeClient.FileChooserParams.parseResult(resultCode, data);
+            }
+        }
+
+        filePathCallback.onReceiveValue(results);
+        filePathCallback = null;
+        cameraPhotoUri = null;
+    }
+
+    private Intent createCameraIntent() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getPackageManager()) == null) {
+            return null;
+        }
+
+        try {
+            File photoFile = createImageFile();
+            cameraPhotoUri = FileProvider.getUriForFile(
+                    this,
+                    getPackageName() + ".fileprovider",
+                    photoFile
+            );
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, cameraPhotoUri);
+            cameraIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            return cameraIntent;
+        } catch (IOException e) {
+            cameraPhotoUri = null;
+            return null;
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.UK).format(new Date());
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile("elite_arrows_result_" + timestamp + "_", ".jpg", storageDir);
     }
 
     @Override
